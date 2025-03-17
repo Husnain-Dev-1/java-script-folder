9bc5f17a0de48d72d235012d13d0552b318ebe5a
Husnain-Dev-1 <naseembhatti5503@gmail.com>
2025-03-17 13:38:18 +0500

    Added three files

---
+++ if_function.js
@@ -0,0 +1,315 @@
+
+
+
+//progrm 1
+
+{
+  let a = 4;
+  b = 20;
+  if (a + b == 20) {
+    console.log("YOU ARE YOUNG ENOUGH TO DRIVE");
+  }
+  else {
+    console.log("You are not able to dive you are underage");
+  }
+}
+
+//program 2
+
+{
+  let a = 30;
+  b = 20;
+  if (a + b >= 40) {
+    console.log("You are able to do this function");
+  }
+  else {
+    console.log("You are not able to do this function");
+  }
+}
+
+//program 3
+
+{
+  let c = 2;
+  d = 3;
+  if (c + d * 10) {
+    console.log("You are drive very good");
+  }
+  else {
+    console.log("You are drive very bad");
+  }
+}
+
+//program 4
+
+{
+  let a = 4;
+  b = 3;
+  if (a + b == 7) {
+    console.log("sister");
+  }
+  else {
+    console.log("brother");
+  }
+}
+
+//program 5
+
+{
+  let g = 2;
+  h = 3;
+  if (g + h - 8) {
+    console.log("The sun sets, painting the sky.");
+  }
+  else {
+    console.log("You are drive very bad");
+  }
+}
+
+// extra 
+
+{
+  let a = 6;
+  b = 6;
+  if (!true) {
+    console.log("logical opration is not start.");
+  }
+  else {
+    console.log("logical opration is start.");
+  }
+}
+
+// logical operations
+
+// And program &&
+
+// program 1 And &&
+
+{
+  let a = 5;
+  b = 5;
+  if (a + b == 10 && a - b == 0) {
+    console.log("The wind whispers through the trees.");
+  }
+  else {
+    console.log(" Stars begin to twinkle above.");
+
+  }
+}
+
+// porgram 2 And &&
+
+{
+  let a = 3;
+  b = 3;
+  if (a + b > 5 && a - b == 0) {
+    console.log("A breeze is blowing.");
+  }
+  else {
+    console.log("Today feels peaceful.");
+  }
+}
+
+
+// program 3 And &&
+
+{
+  let a = 9;
+  b = 7;
+  if (a + b == 16 && a - b <= 2) {
+    console.log("The moon casts a silver glow.");
+  }
+  else {
+    console.log("Waves crash gently on the shore.");
+  }
+}
+
+// program 4 And &&
+
+{
+  let a = 10;
+  b = 8;
+  if (a + b == 18 && a - b == 2) {
+    console.log("A cool breeze fills the night air. Silence settles over the world.");
+  }
+  else {
+    console.log("The sun was setting, painting the sky in shades of orange and pink.");
+
+  }
+}
+
+// program 5 And &&
+
+{
+  let a = 13;
+  b = 2;
+  if (a + b == 15 && a - b == 11) {
+    console.log("A cool breeze rustled the leaves, filling the air with a soft whisper.");
+  }
+  else {
+    console.log(" Birds chirped their evening songs as the world slowly quieted down.");
+  }
+}
+
+// program 6 And &&
+
+{
+  let a = 2;
+  b = 2;
+  if (a + b == 4 && a - b == 1) {
+    console.log("She took a deep breath, savoring the peaceful moment before heading home.");
+  }
+  else {
+    console.log("My name is Husnain.");
+  }
+}
+
+// OR programs ||
+
+// program 1 OR ||
+
+{
+  let a = 10;
+  b = 1;
+  if (a + b == 20 || a - b == 29) {
+    console.log("The sun is shining.");
+  }
+  else {
+    console.log("Birds are singing.");
+  }
+}
+
+// program 2 Or ||
+
+{
+  let a = 3;
+  b = 2;
+  if (a - b == 2 || a + b == 5) {
+    console.log("sun is rising");
+  }
+  else {
+    console.log("Moon is beautiful");
+  }
+}
+
+// program 3 OR ||
+
+{
+  let a = 17;
+  b = 3;
+  if (a + b == 10 || a - b == 9) {
+
+  }
+  else {
+
+  }
+}
+
+// program 4 OR ||
+
+// Not program !
+
+// program 1 Not !
+
+{
+  let a = 5;
+
+  if (!(a > 25)) {
+    console.log("The cat sleeps.");
+  }
+  else {
+    console.log("The dog runs.");
+  }
+}
+
+
+// nested if programs
+
+// program 1 nested
+
+{
+  let email = "isawairaftikar@gmail.com";
+  password = 34587690;
+  myName = "Sawaira";
+
+  if (email == "isawairaftikar@gmail.com") {
+    console.log("Your verification done");
+
+    if (password == 34587690) {
+      // console.log("password is okay ");
+
+      if (myName == "sawaira") {
+        console.log("Your username is correct");
+      } else {
+        console.log("incorrect username");
+      }
+
+    } else {
+      console.log("incorrect passwrd");
+    }
+
+  } else {
+    console.log("Incorrect Email");
+  }
+
+}
+
+// program 2 nested
+
+{
+  let a = 3;
+  if (a = 3) {
+    console.log("I am happy.");
+  } else {
+    console.log("I am not happy.");
+  }
+}
+
+// program 4 nested
+
+{
+  let a = 10;
+  b = 20;
+  if (b == 20) {
+    console.log("Birds chirp outside.");
+
+    if (a == 5) {
+      console.log("The sun is bright.");
+    } else {
+      console.log("I love coffee.");
+    }
+
+  } else {
+    console.log("Let's go outside!");
+  }
+}
+
+// program 5 nested
+
+{
+  let a = 5;
+  b = 10;
+  c = 5;
+  if (a == c) {
+    console.log("The cat sleeps.");
+
+    if ( c == a) {
+      console.log("Rain is falling.");
+
+      if (c == b) {
+        console.log("She smiles warmly.");
+      }
+      else {
+        console.log("Open the door.");
+      }
+    }
+    else {
+      console.log("Stars shine brightly.");
+    }
+  }
+  else {
+    console.log("Time flies fast.");
+  }
+}
+
+// He runs fast.
---
+++ script.js
@@ -0,0 +1,82 @@
+{
+    let a = 10;
+    b = 2;
+    console.log(a + b);
+}
+
+{
+    let myName;
+    console.log(myName);
+}
+
+{
+    let myAge = 14;
+    console.log(myAge);
+    console.log(typeof 14);
+}
+
+// operstions data type 
+
+//   arithmetic
+
+{
+    let a = 2, b = 3
+    console.log(a + b);    // addition 
+    console.log(a - b);    // subtraction 
+    console.log(a * b);    // multplication
+    console.log(a % b);    // rimander
+    console.log(a / b);    // division
+    console.log(a ** b);   // power off
+}
+
+//   asignment
+
+{
+    let x = 15;
+    x += 3;    // addition 
+    console.log(x);
+    x -= 4;    // subtraction     
+    console.log(x);
+    x *= 10;   // multplication
+    console.log(x);
+    x /= 20;   // division
+    console.log(x);
+    x %= 8;    // rimander
+    console.log(x);
+}
+
+//    logical operation
+
+{
+    let
+}
+
+// bitwise
+
+{
+    let
+}
+
+//    ternery
+
+{
+    let
+}
+
+//    type operation
+
+{
+    let
+}
+
+//    nullisn
+
+{
+    let
+}
+
+//   optional
+
+{
+    let
+}
